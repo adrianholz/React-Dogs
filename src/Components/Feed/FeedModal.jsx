f75b749af45914903df15af12130dafe620e12b6
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2024-06-19 12:43:00 -0700

    Feed and post modal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./FeedModal.scss";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTO_GET } from "../../Api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import PhotoContent from "../Photo/PhotoContent";
 
-const FeedModal = () => {
-  return <div></div>;
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  return (
+    <div className="modal" onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./FeedPhotosItem.scss";
 
-const FeedPhotosItem = ({ photo }) => {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
+  }
+
   return (
-    <li className="photo">
+    <li className="photo" onClick={handleClick}>
       <img src={photo.src} alt={photo.title} />
       <span>{photo.acessos}</span>
     </li>
