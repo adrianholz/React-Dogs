24544c02455f76da58fd79d7d56be181729c7db1
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2024-06-19 13:30:45 -0700

    Comments

---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Enviar from "../../Assets/enviar.svg?react";
+import useFetch from "../../Hooks/useFetch";
+import { COMMENT_POST } from "../../Api";
+import Error from "../Helper/Error";
+import "./PhotoCommentsForm.scss";
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = useState("");
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment("");
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form className="commentForm" onSubmit={handleSubmit}>
+      <textarea
+        className="commentTextarea"
+        id="commend"
+        name="commend"
+        placeholder="Comente algo..."
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button className="commentButton">
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
